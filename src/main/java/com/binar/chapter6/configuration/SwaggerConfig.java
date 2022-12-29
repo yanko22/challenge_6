3af3765fc3f02df9c584bdf589008fed4228c146
yanko22 <74333856+yanko22@users.noreply.github.com>
2022-12-29 18:30:13 +0800

    Update SwaggerConfig.java

--- src/main/java/com/binar/chapter6/configuration/SwaggerConfig.java
+++ src/main/java/com/binar/chapter6/configuration/SwaggerConfig.java
@@ -1,16 +1,42 @@
 package com.binar.chapter6.configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI demoApi(@Value("Rest API for Challenge") String appDescription,
                            @Value("v1.0.0") String appVersion) {
+        Server server1 = new Server();
+        server1.setUrl("https://challenge6-production-f5f1.up.railway.app/");
+        Server server2 = new Server();
+        server2.setUrl("http://localhost:8080/");
+        List<Server> listOfServer = new ArrayList<>();
+        listOfServer.add(server1);
+        listOfServer.add(server2);
         return new OpenAPI()
                 .info(new Info()
                         .title("Open API")
@@ -19,7 +45,18 @@ public class SwaggerConfig {
                         .termsOfService("http://swagger.io/terms")
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("http://springdoc.org")));
+                                .url("http://springdoc.org")))
+                .servers(listOfServer);
     }
-}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+}
